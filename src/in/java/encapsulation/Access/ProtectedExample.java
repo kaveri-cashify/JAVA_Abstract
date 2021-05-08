e97b58f0abef6bf94342d69ce4cc9bf4bc4070a5
kaveri-cashify <kaveri.s@cashify.in>
2021-05-08 17:32:58 +0530

    Protected

--- src/in/java/encapsulation/Access/ProtectedExample.java
+++ src/in/java/encapsulation/Access/ProtectedExample.java
@@ -1,5 +1,13 @@
 package in.java.encapsulation.Access;
 
 public class ProtectedExample {
-
+        protected int addTwoNumbers(int a, int b){
+            return a+b;
+        }
+    }
+class Test extends ProtectedExample {
+    public static void main(String args[]) {
+        Test obj = new Test();
+        System.out.println(obj.addTwoNumbers(11, 22));
+    }
 }
