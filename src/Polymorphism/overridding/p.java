d362205312eb12b0bf85d0f650b5e4a181215963
kaveri-cashify <kaveri.s@cashify.in>
2021-05-10 16:03:28 +0530

    polymorphism.Overridding

---
+++ src/Polymorphism/overridding/p.java
@@ -0,0 +1,38 @@
+package Polymorphism.overridding;
+
+    class Parent {
+
+        void Print()
+        {
+            System.out.println("parent class");
+        }
+    }
+
+    class subclass1 extends Parent {
+
+        void Print()
+        {
+            System.out.println("subclass1");
+        }
+    }
+
+    class subclass2 extends Parent {
+
+        void Print()
+        {
+            System.out.println("subclass2");
+        }
+    }
+
+    class TestPolymorphism3 {
+        public static void main(String[] args)
+        {
+
+            Parent a = new subclass1();
+            a.Print();
+
+            Parent b = new subclass2();
+            b.Print();
+        }
+    }
+
